32c7ce84edab328d433a078cb278fed4b787e4a5
William Rena <wilyamx@gmail.com>
2024-02-22 08:52:25 +0800

    Using task groups

--- SwiftConcurrency/SwiftConcurrency/Demo/MyTaskGroup.swift
+++ SwiftConcurrency/SwiftConcurrency/Demo/MyTaskGroup.swift
@@ -8,13 +8,62 @@
 import SwiftUI
 
 class MyTaskGroupManager {
+    let urlString = "https://picsum.photos/300"
+    
+    func fetchImagesWithAsyncLet() async throws -> [UIImage] {
+        async let fetchImage1 = fetchImage(urlString: urlString)
+        async let fetchImage2 = fetchImage(urlString: urlString)
+        async let fetchImage3 = fetchImage(urlString: urlString)
+        async let fetchImage4 = fetchImage(urlString: urlString)
+        
+        let (image1, image2, image3, image4) = await (
+            try fetchImage1,
+            try fetchImage2,
+            try fetchImage3,
+            try fetchImage4
+        )
+        return [image1, image2, image3, image4]
+    }
+    
+    func fetchImagesWithTaskGroup() async throws -> [UIImage] {
+        let urlStrings = [
+            urlString,
+            urlString,
+            urlString,
+            urlString,
+            urlString
+        ]
+        
+        return try await withThrowingTaskGroup(
+            of: UIImage.self,
+            body: { group in
+                var images: [UIImage] = []
+                images.reserveCapacity(urlStrings.count)
+                
+                for urlString in urlStrings {
+                    group.addTask {
+                        try await self.fetchImage(urlString: urlString)
+                    }
+                }
+                
+                for try await image in group {
+                    images.append(image)
+                }
+                
+                return images
+        })
+    }
+    
     private func fetchImage(urlString: String) async throws -> UIImage {
         guard let url = URL(string: urlString) else {
             throw URLError(.badURL)
         }
         
         do {
-            let (data, _) = try await URLSession.shared.data(from: url, delegate: nil)
+            let (data, _) = try await URLSession.shared.data(
+                from: url,
+                delegate: nil
+            )
             if let image = UIImage(data: data) {
                 return image
             }
@@ -30,10 +79,24 @@ class MyTaskGroupManager {
 class MyTaskGroupViewModel: ObservableObject {
     var dataManager: MyTaskGroupManager
     
+    let columns = [GridItem(.flexible()), GridItem(.flexible())]
+    
     @Published var images: [UIImage] = []
     
     init(dataManager: MyTaskGroupManager) {
         self.dataManager = dataManager
+    }
+    
+    func getImages() async {
+        if let images = try? await dataManager.fetchImagesWithAsyncLet() {
+            self.images.append(contentsOf: images)
+        }
+    }
+    
+    func getImagesWithTaskGroup() async {
+        if let images = try? await dataManager.fetchImagesWithTaskGroup() {
+            self.images.append(contentsOf: images)
+        }
     }
 }
 
@@ -43,7 +106,23 @@ struct MyTaskGroup: View {
     )
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: viewModel.columns) {
+                    ForEach(viewModel.images, id: \.self) { image in
+                        Image(uiImage: image)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: 150)
+                    }
+                }
+            }
+            .navigationTitle("Task Group")
+            .task {
+                //await viewModel.getImages()
+                await viewModel.getImagesWithTaskGroup()
+            }
+        }
     }
 }
 
